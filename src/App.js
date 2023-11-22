3d2f6a3c9a763fec1cf13c3b7fd95765f475edc2
Joseph1818 <ujee@UMBAS-MacBook-Air.local>
2023-11-22 22:59:55 +0200

    second commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-
 import "./App.css";
+import React, { useState } from "react";
 import { SearchBar } from "./components/searchBar";
 
 function App() {
+  const [results, setResults] = useState([]);
+
   return (
     <div className="App">
       <div className="search-bar-container">
-        <div className="search-bar-title">WhoLook<span className="find-Span">Find</span></div>
-        <div className="search-bar">
-          <SearchBar />
+        <div className="search-bar-title">
+          WhoLook<span className="find-Span">Find</span>
         </div>
-        <div>SearchBarResults</div>
+        <div className="search-bar">
+          <SearchBar setResults={setResults} />
+        </div>
       </div>
     </div>
   );
--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -1,33 +1,55 @@
 import React, { useState, useEffect } from "react";
-import { FaSearch } from "react-icons/fa";
-import axios from "axios";
+import { FaAlignCenter, FaSearch } from "react-icons/fa";
 import "./searchBar.scss";
 
 export const SearchBar = () => {
   //Storing data passed into the input field.
-  const [input, setInput] = useState("");
 
-  //Making a request to the API using fetch
-  const fetchData = (value) => {
-    fetch("https://api.crossref.org/journals")
-      .then((response) => response.json())
-      .then((json) => {
-        console.log(json);
-      });
-  };
-  const handleChange = (value) => {
-    setInput(value);
-    fetchData(value);
+  //Making a request to the API using axios
+  useEffect(() => {
+    const fetchData = (value) => {
+      fetch("https://api.crossref.org/journals")
+        .then((response) => response.json())
+        .then((json) => {
+          const data = json.message.items;
+          // setData(data);
+          setFilterData(data);
+          // console.log(results);
+        });
+    };
+
+    fetchData();
+  }, []);
+  const [data, setData] = useState([]);
+  const [filterData, setFilterData] = useState();
+  const handleFilter = (value) => {
+    const res = filterData.filter((f) => f.title.toLowerCase().includes(value));
+    setData(res);
   };
 
   return (
-    <div className="input-wrapper">
-      <FaSearch id="search-icon" />
-      <input
-        placeholder="Type to search"
-        value={input}
-        onChange={(e) => handleChange(e.target.value)}
-      />
+    <div>
+      <div className="input-wrapper">
+        <FaSearch id="search-icon" />
+        <input
+          placeholder="Type to search"
+          autoFocus
+          onChange={(e) => handleFilter(e.target.value)}
+        />
+      </div>
+      <div className="results-list">
+        {data.map((d, i) => {
+          return (
+            <div
+              className="search-result"
+              onClick={(e) => alert(`You clicked on ${d.title}`)}
+              key={i}
+            >
+              {d.title}{" "}
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
